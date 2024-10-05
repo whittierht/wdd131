54fbd606c66771e625c8c83cd1f30dc19f521588
Hyrum Whittier <whittierht@gmail.com>
2024-10-05 11:06:18 -0600

    changes to javascript

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,9 +1,32 @@
 function toggleMenu() {
     const menuItems = document.querySelector('.menu-items');
-
     menuItems.classList.toggle('show');
 }
+
+function handleResize() {
+    const menuButton = document.querySelector('.menu-button');
+    const menuItems = document.querySelector('.menu-items');
+
+
+    if (window.innerWidth >= 1000) {
+
+        menuButton.style.display = 'none';
+        menuItems.classList.add('show'); 
+    } else {
+
+        menuButton.style.display = 'inline'; 
+        if (!menuItems.classList.contains('show')) {
+            menuItems.classList.remove('show'); // 
+        }
+    }
+}
+
 
 const menuButton = document.querySelector('.menu-button');
 menuButton.addEventListener('click', toggleMenu);
 
+
+window.addEventListener('resize', handleResize);
+
+
+handleResize();
